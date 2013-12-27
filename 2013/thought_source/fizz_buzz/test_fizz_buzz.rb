e50b8e2ed090c61622c7ee534f1664ec1bfb21cc
Lincoln Lee <git@lincoln.hk>
2013-12-27 13:03:23 +0800

    Change test name

--- 2013/thought_source/fizz_buzz/test_fizz_buzz.rb
+++ 2013/thought_source/fizz_buzz/test_fizz_buzz.rb
@@ -17,11 +17,11 @@ class TestFizzBuzz < Minitest::Test
     assert_equal "Buzz", @fizzbuzz.buzz
   end
 
-  def test_it_will_print_fizz_on_3
+  def test_it_will_fizz_on_3
     assert_equal "Fizz", @fizzbuzz.print(3)
   end
 
-  def test_it_will_print_buzz_on_5
+  def test_it_will_buzz_on_5
     assert_equal "Buzz", @fizzbuzz.print(5)
   end
 
