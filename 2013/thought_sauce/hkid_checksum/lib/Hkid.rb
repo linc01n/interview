bf9bb135326bc9c689da8148334b9ff4eeb55c84
Lincoln Lee <git@lincoln.hk>
2013-12-28 20:26:04 +0800

    Pass basic check digit test

--- 2013/thought_sauce/hkid_checksum/lib/Hkid.rb
+++ 2013/thought_sauce/hkid_checksum/lib/Hkid.rb
@@ -2,5 +2,17 @@
 
 class Hkid
   def check_digit(hkid)
+    sum = 0
+    leading_char = []
+
+    hkid.downcase.reverse.split(//).each_with_index do |char, index|
+      if index < 6
+        sum += char.to_i * (index + 2)
+      else
+        sum += (char.ord - 87) * (index + 2)
+      end
+    end
+
+    11 - sum % 11
   end
 end
